3932c2dd744b715d0853c1fcf846ec6a2a4b96cc
Julio Antonio <julioam766@gmail.com>
2024-02-09 02:50:15 +0700

    new file:   .vscode/settings.json 	modified:   Best Divisor.py

--- Best Divisor.py
+++ Best Divisor.py
@@ -5,6 +5,18 @@ import re
 import sys
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    n = int(input().strip()) 
+
 def bestdivisorofn(n):
-    return n
+    factor_list = []
+    for fac1 in range(1, (n//2)+1): 
+        if n%fac1 == 0: 
+            fac2 = n//fac1 
+            f1 = sum(list(map(int, str([fac1][0])))) 
+            f2 = sum(list(map(int, str([fac2][0]))))    
+            factor_list.append(max(f1, f2))
+        else:
+            pass  
+    return max(factor_list) 
+#testig 
+bestdivisorofn(24)
